ada0e902c4a63bf1dadec287c2bbd8c036468113
rjsdnql123 <rjsdnql123@gmail.com>
2023-12-13 14:15:00 +0900

    (chore): eslint prettier 셋팅

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,6 +1,3 @@
-import Link from "next/link";
-import { Metadata } from "next";
-
-export default async function Page() {
-  return <div>main Pagadfsfdase</div>;
+export default function Page() {
+  return <div>main </div>;
 }
